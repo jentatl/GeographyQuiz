ceb6e3d2ee54bebde7e21e63dea1b7d165c42a1b
harlibott <harlibott@gmail.com>
2019-03-31 13:12:41 -0400

    Quiz Screen and DB stuff

---
+++ app/src/main/java/edu/cs/uga/geographyquiz/Country.java
@@ -0,0 +1,41 @@
+package edu.cs.uga.geographyquiz;
+
+public class Country {
+
+    private int countryId;
+    private String countryName;
+    private String countryContinent;
+
+    //constructors
+    public Country(){}
+    public Country(int id, String country, String continent){
+        this.countryId = id;
+        this.countryName = country;
+        this.countryContinent = continent;
+    }
+
+    //properties
+    public void setId(int id){
+        this.countryId = id;
+    }
+
+    public int getId(){
+        return this.countryId;
+    }
+
+    public void setContinent(String continent){
+        this.countryContinent = continent;
+    }
+
+    public String getContinent(){
+        return this.countryContinent;
+    }
+
+    public void setCountryName(String name){
+        this.countryName = name;
+    }
+
+    public String getCountryName(){
+        return this.countryName;
+    }
+}
