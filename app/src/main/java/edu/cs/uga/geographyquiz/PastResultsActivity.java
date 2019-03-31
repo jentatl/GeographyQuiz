27e29596c923d228aedcb756a84e25d39007a167
jentatl <jentatl@users.noreply.github.com>
2019-03-31 13:17:53 -0400

    splash screen added

--- app/src/main/java/edu/cs/uga/geographyquiz/PastResultsActivity.java
+++ app/src/main/java/edu/cs/uga/geographyquiz/PastResultsActivity.java
@@ -1,13 +1,14 @@
 package edu.cs.uga.geographyquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PastResultsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_past_results);
     }
 }
